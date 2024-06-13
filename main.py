632e00a41e79905e43341152f829db5e25b7f490
roger <bian14r.tech@gmail.com>
2024-06-13 12:53:47 +0900

    adjust overlap timekeeping logic + fps info printout

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import easyocr
 
 
 cap = cv2.VideoCapture("data/clipped.MP4")
-fps = int(cap.get(cv2.CAP_PROP_FPS))
+fps = round(cap.get(cv2.CAP_PROP_FPS))
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -32,6 +32,7 @@ def check_overlap(l1, r1, l2, r2):
 
 
 n = 0
+counter = 0
 while True:
     ret, frame = cap.read()
     if ret:
@@ -39,9 +40,19 @@ while True:
         img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         img_pil = Image.fromarray(img)
         
-        # write fps & frame count
-        cv2.putText(img, 'frame: '+str(n+1), (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
-        cv2.putText(img, str(fps)+' fps', (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+        # ocr detection
+        d = reader.readtext(img, min_size=5, text_threshold=0.3, mag_ratio=2, allowlist='0123456789')
+        
+        if len(d) > 0:
+            for i in d:
+                if i[-1] > 0.8:
+                    l = int(i[0][0][0]) - 10
+                    r = int(i[0][2][0]) + 10
+                    t = int(i[0][0][1]) - 10
+                    b = int(i[0][2][1]) + 10
+                    
+                    cv2.rectangle(img, (l, t), (r, b), (0, 0, 255), 2)
+                    cv2.putText(img, i[1], (l, t - 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2, cv2.LINE_AA)
         
         # tank (hardcoded for now)
         tank_lt = (575, 420)
@@ -63,9 +74,7 @@ while True:
                     cv2.rectangle(img, human_lt, human_rb, (255, 0, 0), 5)
         
         # check overlap & draw arrow & count frame
-        counter = 0
         overlap = check_overlap(tank_lt, tank_rb, human_lt, human_rb)
-        print(overlap)
         if overlap:
             tank_center = (int((tank_lt[0]+tank_rb[0])/2), int((tank_lt[1]+tank_rb[1])/2))
             human_center = (int((human_lt[0]+human_rb[0])/2), int((human_lt[1]+human_rb[1])/2))
@@ -73,23 +82,16 @@ while True:
             cv2.arrowedLine(img, human_center, tank_center, (255, 165, 0), 5, tipLength=0.2)
             
             # count once per second
-            if n // fps == 0:
+            if n % fps == 0:
                 counter += 1
-                cv2.putText(img, str(counter)+' sec', (human_rb[0]+20, human_lt[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 165, 0), 2, cv2.LINE_AA)
+            cv2.putText(img, str(counter)+' sec', (human_rb[0]+20, human_lt[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 165, 0), 2, cv2.LINE_AA)
+        else:
+            counter = 0
                 
-        # ocr detection
-        d = reader.readtext(img, min_size=5, text_threshold=0.3, mag_ratio=2, allowlist='0123456789')
+        # write fps & frame count
+        cv2.putText(img, 'frame: '+str(n+1), (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        cv2.putText(img, str(fps)+' fps', (20, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
         
-        if len(d) > 0:
-            for i in d:
-                if i[-1] > 0.8:
-                    l = int(i[0][0][0]) - 10
-                    r = int(i[0][2][0]) + 10
-                    t = int(i[0][0][1]) - 10
-                    b = int(i[0][2][1]) + 10
-                    
-                    cv2.rectangle(img, (l, t), (r, b), (0, 0, 255), 2)
-                    cv2.putText(img, i[1], (l, t - 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2, cv2.LINE_AA)
         
         img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
         writer.write(img)
